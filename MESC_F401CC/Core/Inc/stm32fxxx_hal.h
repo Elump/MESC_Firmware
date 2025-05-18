4d18e315e4059b88715e31450715bf782466cf68
Elump <41876418+Elump@users.noreply.github.com>
2025-05-18 19:27:59 +0200

    fixt mosfet temp measuremnt

--- MESC_F401CC/Core/Inc/stm32fxxx_hal.h
+++ MESC_F401CC/Core/Inc/stm32fxxx_hal.h
@@ -38,7 +38,7 @@ Profile defaults
 /* Temperature parameters */
 #define MESC_TEMP_MOS_R_F     4700.0f //changed for IHM08
 #define MESC_TEMP_MOS_METHOD  TEMP_METHOD_STEINHART_HART_BETA_R
-#define MESC_TEMP_MOS_SCHEMA  TEMP_SCHEMA_R_F_ON_R_T
+#define MESC_TEMP_MOS_SCHEMA  TEMP_SCHEMA_R_T_ON_R_F
 #define MESC_TEMP_MOS_SH_BETA 3437.864258f
 #define MESC_TEMP_MOS_SH_R    0.098243f
 #define MESC_TEMP_MOS_SH_R0   10000.0f
